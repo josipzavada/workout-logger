ce7e4f5d7d476bf7e0f564c4a1098e0d3578ef0d
josipzavada <josip.zavada@icloud.com>
2024-09-28 13:27:40 +0200

    Add models, move WorkoutLogView to a folder

---
+++ Workout logger/Models/Models.swift
@@ -0,0 +1,44 @@
+//
+//  Models.swift
+//  Workout logger
+//
+//  Created by Josip Zavada on 28.09.2024..
+//
+
+import Foundation
+
+enum WorkoutTarget {
+    case maximum
+    case percentageOfMaximum(Int)
+    case exact(Int)
+    case interval(Int, Int)
+}
+
+enum WorkoutType {
+    case pyramid
+    case emom
+    case superSet
+    case test
+}
+
+enum VolumeUnit {
+    case rep
+    case calorie
+    case distance
+}
+
+struct WorkoutPlanItem {
+    let type: WorkoutType
+    let workouts: [Workout]
+}
+
+struct Workout {
+    let name: String
+    let sets: [WorkoutSet]
+}
+
+struct WorkoutSet {
+    let volumeUnit: VolumeUnit
+    let targetVolume: WorkoutTarget
+    let targetWeight: WorkoutTarget?
+}
---
+++ Workout logger/Screens/WorkoutLog/WorkoutLogViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  WorkoutLogViewModel.swift
+//  Workout logger
+//
+//  Created by Josip Zavada on 28.09.2024..
+//
+
